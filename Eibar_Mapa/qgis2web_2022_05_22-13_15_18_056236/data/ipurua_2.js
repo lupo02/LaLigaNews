f0bd2d59c3abd3d4fc00f61476f01a5cf6eb0e32
Gabriel Arroyo <99285135+lupo02@users.noreply.github.com>
2022-05-22 13:39:28 +0200

    Add files via upload

---
+++ Eibar_Mapa/qgis2web_2022_05_22-13_15_18_056236/data/ipurua_2.js
@@ -0,0 +1,1 @@
+var json_ipurua_2 = {"type":"FeatureCollection","name":"ipurua_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"8"},"geometry":{"type":"Point","coordinates":[-2.47580524026902,43.181828653652005]}}]}
